f2defbd19a13811434b8d2f434ecae36951710d3
kabita <buyokavita@gmail.com>
2022-01-24 11:32:29 +0545

    completedshape

---
+++ new/script.js
@@ -0,0 +1,22 @@
+// ES5
+
+var num = 1;
+var string = "hs";
+console.log(num, string);
+
+var array = [1, "habi", 0.4];
+console.log(array.length);
+
+var object = { name: "kabita", age: 2 };
+console.log(object.name, object.age);
+
+//ES6
+const num1 = 1;
+const string1 = "hs";
+console.log(num1, string1);
+
+const array1 = [1, "habi", 0.4];
+console.log(array1.length);
+
+const object1 = { name: "kabita", age: 2 };
+console.log(object1.name, object1.age);
